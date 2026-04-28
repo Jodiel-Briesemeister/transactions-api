47cc8fe06e09159e77159bf31bb1f9b1ebd0d4db
Jodiel Briesemeister <jodielll29@gmail.com>
2026-04-28 03:46:57 -0300

    style: remove unnecessary line breaks

--- migrations/20260422233641_create_users_table.ts
+++ migrations/20260422233641_create_users_table.ts
@@ -3,13 +3,9 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('users', (table) => {
     table.uuid('id').primary();
-
     table.string('name').notNullable().index();
-
     table.string('email').notNullable().unique().index();
-
     table.string('password_hash').notNullable();
-
     table.timestamps(true, true);
   });
 }
